b1296736fa7f43b0901410be15a930ffd58edb46
fatah <yudafatah@gmail.com>
2021-01-01 02:12:00 +0700

    add mongodb

---
+++ Identity/internal/databases/databases.go
@@ -0,0 +1,10 @@
+/*
+ * @File: databases.databases.go
+ * @Description: Creates global database instance
+ */
+ package databases
+
+ // Database shares global database instance
+ var (
+	 Database MongoDB
+ )
---
+++ Identity/internal/databases/mongodb.go
@@ -0,0 +1,79 @@
+/*
+ * @File: databases.mongodb.go
+ * @Description: Handles MongoDB connections
+ */
+ package databases
+
+ import (
+	 "time"
+	 "github.com/yudafatah/golang-microservices-boilerplate/tree/main/Identity/common"
+	 "github.com/yudafatah/golang-microservices-boilerplate/tree/main/Identity/internal/models"
+	 log "github.com/sirupsen/logrus"
+	 mgo "gopkg.in/mgo.v2"
+	 "gopkg.in/mgo.v2/bson"
+ )
+ 
+ // MongoDB manages MongoDB connection
+ type MongoDB struct {
+	 MgDbSession  *mgo.Session
+	 Databasename string
+ }
+ 
+ // Init initializes mongo database
+ func (db *MongoDB) Init() error {
+	 db.Databasename = common.Config.MgDbName
+ 
+	 // DialInfo holds options for establishing a session with a MongoDB cluster.
+	 dialInfo := &mgo.DialInfo{
+		 Addrs:    []string{common.Config.MgAddrs}, // Get HOST + PORT
+		 Timeout:  60 * time.Second,
+		 Database: db.Databasename,            // Database name
+		 Username: common.Config.MgDbUsername, // Username
+		 Password: common.Config.MgDbPassword, // Password
+	 }
+ 
+	 // Create a session which maintains a pool of socket connections
+	 // to the DB MongoDB database.
+	 var err error
+	 db.MgDbSession, err = mgo.DialWithInfo(dialInfo)
+ 
+	 if err != nil {
+		 log.Debug("Can't connect to mongo, go error: ", err)
+		 return err
+	 }
+ 
+	 return db.initData()
+ }
+ 
+ // InitData initializes default data
+ func (db *MongoDB) initData() error {
+	 var err error
+	 var count int
+ 
+	 // Check if user collection has at least one document
+	 sessionCopy := db.MgDbSession.Copy()
+	 defer sessionCopy.Close()
+ 
+	 // Get a collection to execute the query against.
+	 collection := sessionCopy.DB(db.Databasename).C(common.ColUsers)
+	 count, err = collection.Find(bson.M{}).Count()
+ 
+	 if count < 1 {
+		 // Create admin/admin account
+		 var user models.User
+		 user = models.User{bson.NewObjectId(), "last", "first", "admin", 
+		 time.Time(time.Now().Local().UTC()), time.Time(time.Now().Local().UTC()), "xxx", "xxx", 
+		"admin@gmail.com", "california", "i'm admin", "+628888", "USA", true, false, "img.com/img.png",
+		"img.com/img.png"}
+		 err = collection.Insert(&user)
+	 }
+ 
+	 return err
+ }
+ 
+ // Close the existing connection
+ func (db *MongoDB) Close() {
+	 if db.MgDbSession != nil {
+		 db.MgDbSession.Close()
+	 }
+ }
